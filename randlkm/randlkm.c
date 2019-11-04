e540f8adc1ba69ca16bf6ba9d042e34deb97a55e
auduongtansang <1712145@student.hcmus.edu.vn>
2019-11-04 14:36:18 +0700

    Add comments

--- randlkm/randlkm.c
+++ randlkm/randlkm.c
@@ -1,9 +1,9 @@
-#include <linux/random.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/uaccess.h>
-#include <linux/miscdevice.h>
+#include <linux/random.h>  //get_random_bytes()
+#include <linux/module.h>  //...
+#include <linux/fs.h>  //file_operations structure
+#include <linux/init.h>  //module_init(), module_exit()
+#include <linux/uaccess.h>  //copy_to_user()
+#include <linux/miscdevice.h>  //miscdevice structure
 
 MODULE_AUTHOR("Au Duong Tan Sang 1712145");
 MODULE_DESCRIPTION("Random number LKM");
@@ -11,8 +11,10 @@ MODULE_LICENSE("GPL");
 
 char data[512] = "";
 
+//Numbers to characters
 void NumToChar(unsigned int num, char cha[])
 {
+    //Put numbers to a character array
     int cnt = 0, i, j;
     do
     {
@@ -20,6 +22,7 @@ void NumToChar(unsigned int num, char cha[])
         num /= 10;
     } while (num != 0);
     
+    //Reverse the array
     i = 0;
     j = cnt - 1;
     while (i < j)
@@ -30,15 +33,19 @@ void NumToChar(unsigned int num, char cha[])
         i++; j--;
     }
     
+    //End of the array
     cha[cnt] = '\0';
 }
 
+//Occur when user applications call read()
 ssize_t user_read(struct file *fp, char *buf, size_t cnt, loff_t *of)
 {
+    //Generate some random bytes and change them to characters
     unsigned int number = 0;
     get_random_bytes(&number, sizeof(number));
     NumToChar(number, data);
 
+    //Send those characters to user application
     if (copy_to_user(buf, data, strlen(data)) != 0)
     {
         printk("copy_to_user(): fatal error.\n");
@@ -47,11 +54,13 @@ ssize_t user_read(struct file *fp, char *buf, size_t cnt, loff_t *of)
     return strlen(data);
 }
 
+//We only use "read" operation
 static struct file_operations randops =
 {
     .read = user_read
 };
 
+//Identity of device file
 static struct miscdevice randdev =
 {
     .minor = MISC_DYNAMIC_MINOR,
@@ -59,15 +68,19 @@ static struct miscdevice randdev =
     .fops = &randops
 };
 
+//LKM loader
 static int __init rand_init(void)
 {
+    //Register device file
     misc_register(&randdev);
     printk("Loaded.\n");
     return 0;
 }
 
+//LKM unloader
 static void __exit rand_exit(void)
 {
+    //Unregister device file
     misc_deregister(&randdev);
     printk("Unloaded.\n");
     return;
