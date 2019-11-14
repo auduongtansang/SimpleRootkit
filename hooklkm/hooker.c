fffbae301f7ddcca1c326f6e8dbf82a759fb4eb9
auduongtansang <1712145@student.hcmus.edu.vn>
2019-11-14 13:04:20 +0700

    Disable/Enable protection

--- hooklkm/hooker.c
+++ hooklkm/hooker.c
@@ -1,13 +1,45 @@
 #include <linux/module.h>  //...
 #include <linux/init.h>  //module_init(), module_exit()
+#include <linux/kallsyms.h>  //kallsyms_lookup_name()
 
 MODULE_AUTHOR("Au Duong Tan Sang 1712145");
 MODULE_DESCRIPTION("Simple hooker LKM");
 MODULE_LICENSE("GPL");
 
+//sys_call_table address
+void **sys_call_table_addr;
+
+//Make the page writable
+void make_page_rw(unsigned long addr)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address(addr, &level);
+    if (pte->pte & ~_PAGE_RW)
+        pte->pte |= _PAGE_RW;
+}
+
+//Make the page read-only
+void make_page_ro(unsigned long addr)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address(addr, &level);
+    pte->pte &= ~_PAGE_RW;
+}
+
 //LKM loader
 static int __init hooker_init(void)
 {
+    //Looking for address of sys_call_table
+    sys_call_table_addr = (void*)kallsyms_lookup_name("sys_call_table");
+
+    //Disable write-protection
+    write_cr0(read_cr0() & (~0x10000));
+    make_page_rw((unsigned long)sys_call_table_addr);
+
+    //Re-enable write-protection
+    write_cr0(read_cr0() | 0x10000);
+    make_page_ro((unsigned long)sys_call_table_addr);
+
     printk("[+] hooker loaded.\n");
     return 0;
 }
